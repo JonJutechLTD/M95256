7e3790608153ed6906f9890b9836a0ca612719ed
JonJutechLTD <91469868+JonJutechLTD@users.noreply.github.com>
2021-09-27 14:22:24 +0100

    Update M95M01.h

--- M95M01.h
+++ M95M01.h
@@ -1,9 +1,9 @@
 /*******************************************************************************
 
- M95M01 SPI EEPROM library
+ M95256 SPI EEPROM library
  -------------------------
  
- M95M01.h - M95M01 class header file
+ M95256.h - M95256 class header file
  
  Code written by Stefan Dzisiewski-Smith.
  
@@ -31,27 +31,27 @@
 
 *******************************************************************************/
 
-#ifndef M95M01_H
-#define M95M01_H
+#ifndef M95256_H
+#define M95256_H
 
 #include <SPI.h>
 
 
-class M95M01_t
+class M95256_t
 {		
 	public:
-		M95M01_t();
+		M95256_t();
 		void begin(uint8_t CS_pin, uint32_t speed_Hz);
 		uint8_t write_byte(uint32_t address, uint8_t value);
 		uint8_t read_byte(uint32_t address);
 		uint8_t write_array(uint32_t address, uint8_t value_array[], const uint32_t array_length);
 		uint8_t read_array(uint32_t address, uint8_t value_array[], const uint32_t array_length);
-		static const uint16_t page_size = 256;  // bytes per page
+		static const uint16_t page_size = 64;  // bytes per page
 		static const uint16_t num_pages = 512;
 		static const uint32_t num_bytes = (uint32_t)page_size*(uint32_t)num_pages;
 
 
-	// functions / variables internal to the M95M01_t class - you cannot access these externally
+	// functions / variables internal to the M95256_t class - you cannot access these externally
 
 	private:
 		// internal helper functions and variables
@@ -63,6 +63,6 @@ class M95M01_t
 
 };
 
-extern M95M01_t M95M01;
+extern M95256_t M95256;
 
-#endif // M95M01_H
+#endif // M95256_H
